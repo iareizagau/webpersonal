7b4509dda1fa71e3895cda1d79106dd6a4f0af2b
Imanol <iareizagau@gmail.com>
2022-05-12 14:04:56 +0200

    primer commit

---
+++ webpersonal/core/views.py
@@ -0,0 +1,14 @@
+from cgitb import html
+from curses.ascii import HT
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+    
+def about(request):
+    return render(request, "core/about.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
